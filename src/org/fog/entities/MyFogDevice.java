04564323a20ea6e702ff45e909988b28df665bb3
DawnSpider96 <natsukihonnda@gmail.com>
2025-01-19 00:37:06 +0000

    MyBestFitHeuristic finally done sanity checking. Praise the Lord

--- src/org/fog/entities/MyFogDevice.java
+++ src/org/fog/entities/MyFogDevice.java
@@ -280,10 +280,10 @@ public class MyFogDevice extends FogDevice {
 			}
 			// Simon (170125) says VmId will always have a value under OnlinePOC
 			// because operator cannot be null
-			if(tuple.getModuleCopyMap().containsKey(tuple.getDestModuleName()) &&
-					tuple.getModuleCopyMap().get(tuple.getDestModuleName()) != vmId) {
-				return;
-			}
+//			if(tuple.getModuleCopyMap().containsKey(tuple.getDestModuleName()) &&
+//					tuple.getModuleCopyMap().get(tuple.getDestModuleName()) != vmId) {
+//				return;
+//			}
 			tuple.setVmId(vmId);
 			tuple.addToTraversedMicroservices(getId(), tuple.getDestModuleName());
 
